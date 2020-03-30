7493507744ef4fb7eb6a8d8d3cdbe0cb8d708742
vMan <42190753+vmanch@users.noreply.github.com>
2020-03-31 01:32:43 +0200

    Whoops... forgot the take all certs bit
    added from line 232 to 245

--- vCenterDRS2vROPSImporter.ps1
+++ vCenterDRS2vROPSImporter.ps1
@@ -229,6 +229,20 @@ $LogFileLoc = $ScriptPath + '\Log\Logfile.log'
 $LogFileLoc = $ScriptPath + '\Log\Logfile.log'
 Reset-Log -fileName $LogFileLoc -filesize 10mb -logcount 5
 
+#Take all certs.
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 if($creds -gt ""){
 
